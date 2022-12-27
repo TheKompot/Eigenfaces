e8530aeb9306fa865cdac1d3e2d4b6fee4254bc6
rudo <rudolfnosek.rn@gmail.com>
2022-12-27 11:48:34 +0100

    changes structure and implemented methods

--- eigenfaces.py
+++ eigenfaces.py
@@ -1,25 +1,66 @@
 import numpy as np
 
+class NotFittedError(Exception):
+    ''' Raised when model is asked to do something(e.g. predict) before it was fitted '''
+    pass
+
 class EigenfacesInterface:
     
     def fit(self):
         pass
 
+    def transform(self):
+        pass
+
     def predict(self):
         pass
 
-class Clustering(EigenfacesInterface):
+class DimensionalityReduction(EigenfacesInterface):
+    
+    def fit(self,X:np.array, k:int):
+        X = X.T # transpose so one column is one image
+
+        self.avg_vector = np.mean(X,axis=1).reshape(1,-1) # average vector 
+
+        X = X - self.avg_vector.T # normalized images to the center
+
+        #eigenvalues and eigenvectors for m x m cov matrix
+        e_val, e_vec = np.linalg.eig(X.T@X)
+
+        #eigenvectors for n^2 x n^2 cov matrix
+        e_vec = X@e_vec
+
+        e_vec = e_vec[:,:k]
+
+        self.e_vec = e_vec
+    
+    def transform(self,x:np.array) -> np.array:
+
+        x = x - self.avg_vector
+
+        return x@self.e_vec
+    
+    def predict(self):
+        pass
+
+
+class Clustering(DimensionalityReduction):
 
     def __init__(self, n_clusters:int):
-        pass
+        self.n_clusters = n_clusters
+        self._clusters = None
     
     def fit(self, X:np.array):
         pass
 
     def predict(self, x:np.array) -> np.array:
+        if self._clusters is None:
+            raise NotFittedError('Needs to be first fitted to the trainning data')
         pass
 
-class Classifier(EigenfacesInterface):
+        
+
+class Classifier(DimensionalityReduction):
 
     def __init__(self):
         pass
